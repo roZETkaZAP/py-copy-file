bf7b0630a79485be7d66736b567076c5a358bd9c
Igor <zalugin.iv2@gmail.com>
2026-04-17 13:29:11 +0200

    requested changes added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
+import os
+
+
 def copy_file(command: str) -> None:
     part = command.split()
     if len(part) != 3 or part[0] != "cp" or part[1] == part[2]:
         return
+    src = part[1]
+    dst = part[2]
+
+    if os.path.basename(src) != src or os.path.basename(dst) != dst:
+        return
     try:
-        with open(part[1], "r") as src, open(part[2], "w") as copy:
-            for line in src:
-                copy.write(line)
+        with open(src, "r") as file_in, open(dst, "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
     except FileNotFoundError:
         return
