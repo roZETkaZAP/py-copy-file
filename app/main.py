6f6a3cc56a1e2979a203b20494a9614e7ed93a6a
Igor <zalugin.iv2@gmail.com>
2026-04-17 12:49:56 +0200

    all test passed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    part = command.split()
+    if len(part) != 3 or part[0] != "cp" or part[1] == part[2]:
+        return
+    try:
+        with open(part[1], "r") as src, open(part[2], "w") as copy:
+            for line in src:
+                copy.write(line)
+    except FileNotFoundError:
+        return
